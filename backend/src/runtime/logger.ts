bcb2cb16d868616a421276e3e75cc064b5357de0
Hiandy98 <mangoandy98@hotmail.com>
2026-09-19 20:52:23 +0800

    feat: add logger middleware using pino for request logging

---
+++ backend/src/runtime/logger.ts
@@ -0,0 +1,30 @@
+import { createMiddleware } from "hono/factory";
+import pino, {type Logger} from "pino";
+
+
+const baseLogger = pino({
+  transport: {
+    target: "pino-pretty",
+    options: {colorize: true}
+  }
+})
+
+
+type LoggerEnv = {
+  Variables: {
+    logger: Logger;
+  }
+};
+
+
+export const loggerMiddleware = createMiddleware<LoggerEnv>(
+  async (c, next) => {
+    const reqLogger = baseLogger.child({
+      method: c.req.method,
+      path: c.req.path,
+    });
+
+  c.set('logger', reqLogger);
+
+  await next();
+});
