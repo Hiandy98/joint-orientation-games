8c68b4dceaf1e943d3b0fd4a6c59148f0d2acb6d
Hiandy98 <mangoandy98@hotmail.com>
2026-09-20 20:21:43 +0800

    refactor: bootstrap function and update app to use HonoEnv type

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,16 @@
-import { serve } from '@hono/node-server'
+import { serve } from "@hono/node-server"
 import { Hono } from 'hono'
-import type { Logger } from 'pino'
+import { EventEmitter } from "node:events"
 
 import { loggerMiddleware, baseLogger } from './runtime/logger.js'
 import { config } from './runtime/config.js'
-import { connectDatabase, disconnectDatabase } from './runtime/db.js'
+import { connectDatabase, disconnectDatabase, db } from './runtime/db.js'
+import { ServiceRegistry } from './runtime/ServiceRegistry.js'
+import { type PluginContext } from './contracts/PluginContext.js'
+import { PluginLoader } from './runtime/loader.js'
+import { type HonoEnv } from './utils/HonoEnv.js'
 
-type Env = {
-  Variables: {
-    logger: Logger
-  }
-};
-
-const app = new Hono<Env>();
+const app = new Hono<HonoEnv>();
 
 app.use('*', loggerMiddleware);
 
@@ -25,8 +23,27 @@ app.get('/', (c) => {
   return c.text('Hello Hono!');
 })
 
-const startServer = async () => {
+async function bootstrap() {
+  baseLogger.info("Starting joint-orientation-games backend")
+
+  const eventBus = new EventEmitter({ captureRejections: true })
+
+  eventBus.on("error", (err) => {
+    baseLogger.error(err, "EventBus: Uncaught exception in global event bus")
+  })
+
   await connectDatabase();
+
+  const ctx: PluginContext = {
+    services: new ServiceRegistry(baseLogger),
+    log: baseLogger,
+    bus: eventBus,
+    db: db,
+  }
+
+  const loader = new PluginLoader(app, ctx)
+
+  await loader.loadFromDir("./dist/plugins")
 
   const server = serve({
     fetch: app.fetch,
@@ -37,16 +54,20 @@ const startServer = async () => {
 
   const shutdown = async (signal: string) => {
     baseLogger.warn(`Signal ${signal} received; initiating slow-start procedure...`)
-    
+
     server.close()
+    await loader.unloadAll()
     await disconnectDatabase()
     
     baseLogger.info('Service has been safely terminated. Goodbye')
     process.exit(0)
   }
 
-  process.on('SIGTERM', () => shutdown('SIGTERM'))
-  process.on('SIGINT', () => shutdown('SIGINT'))
+  process.on("SIGINT", () => shutdown("SIGINT"))
+  process.on("SIGTERM", () => shutdown("SIGTERM"))
 }
 
-startServer()
+bootstrap().catch((err) => {
+  baseLogger.fatal(err, "A critical, fatal error occurred during system startup!!!!!!!!")
+  process.exit(1)
+})
