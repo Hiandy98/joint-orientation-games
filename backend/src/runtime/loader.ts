4270193cf6806cc4c90c7b08f19bff10948f3d4f
Hiandy98 <mangoandy98@hotmail.com>
2026-09-20 19:53:48 +0800

    refactor: simplify plugin loading process by utilizing PluginScan and PluginBootstrapper

--- backend/src/runtime/loader.ts
+++ backend/src/runtime/loader.ts
@@ -1,13 +1,11 @@
 import { Hono } from "hono"
 import * as fs from "node:fs/promises"
-import * as path from "node:path"
 
 import { type PluginContext } from "../contracts/PluginContext.js"
-import { BasePlugin } from "../contracts/BasePlugin.js"
-import { TopoSort } from "../utils/TopoSort.js"
+import { PluginScan } from "../utils/PluginScan.js"
+import { PluginBootstrapper } from "../utils/PluginBootstrapper.js";
 
 export class PluginLoader {
-  private loadedPlugins: BasePlugin[] = []
   private app: Hono
   private ctx: PluginContext
 
@@ -17,76 +15,13 @@ export class PluginLoader {
   }
 
   public async loadFromDir(dirPath: string): Promise<void> {
+    const registry = new PluginScan(this.ctx);
+    const bootstrapper = new PluginBootstrapper(this.app, this.ctx);
+    
     const entries = await fs.readdir(dirPath, { withFileTypes: true }).catch(() => []);
-    const classMap = new Map<string, typeof BasePlugin>();
-    const depMap = new Map<string, string[]>();
-
     for (const entry of entries) {
-      if (entry.isDirectory()) await this.loadPluginClass(dirPath, entry.name, classMap, depMap);
+      if (entry.isDirectory()) await registry.scanFolder(dirPath, entry.name);
     }
-    await this.initializeAll(new TopoSort(depMap).sort(), classMap);
-    await this.activateAllReady();
-  }
-
-  private async loadPluginClass(dir: string, folder: string, classMap: Map<string, any>, depMap: Map<string, any>): Promise<void> {
-    const entryFile = path.join(dir, folder, "index.js");
-    try {
-      const module = await import(entryFile);
-      this.validateAndRegister(module.default, folder, classMap, depMap);
-    } catch (err) {
-      this.ctx.log.error(err, `Loader: Unable to import plugin file: ${entryFile}`);
-    }
-  }
-
-  private validateAndRegister(PluginClass: any, folder: string, classMap: Map<string, any>, depMap: Map<string, any>): void {
-    if (!PluginClass || !(PluginClass.prototype instanceof BasePlugin)) {
-      return this.ctx.log.warn(`Loader: Skip exporting invalid add-ons: ${folder}`);
-    }
-    const id = PluginClass.pluginId;
-    if (!id) return this.ctx.log.error(`Loader: Plugin undefined, static pluginId: ${folder}`);
-
-    classMap.set(id, PluginClass);
-    depMap.set(id, PluginClass.dependsOn || []);
-  }
-
-  private async initializeAll(sortedIds: string[], classMap: Map<string, typeof BasePlugin>): Promise<void> {
-    for (const id of sortedIds) {
-      const PluginClass = classMap.get(id);
-      if (PluginClass) await this.bootPlugin(id, PluginClass);
-    }
-  }
-
-  private async bootPlugin(id: string, PluginClass: typeof BasePlugin): Promise<void> {
-    const name = PluginClass.name || id;
-    try {
-      this.ctx.log.info(`Loader: Initializing plugin: ${name} (${id})`);
-      const instance = new (PluginClass as any)(this.ctx) as BasePlugin;
-      await instance.init();
-      this.registerRoute(id, instance);
-    } catch (err) {
-      this.ctx.log.error(err, `Loader: Failed to start plugin [${id}]`);
-      throw err;
-    }
-  }
-
-  private registerRoute(id: string, instance: BasePlugin): void {
-    this.app.route(`/api/${id}`, instance.router);
-    this.loadedPlugins.push(instance);
-    this.ctx.log.info(`Loader: Route automatically registered: /api/${id}/*`);
-  }
-
-  private async activateAllReady(): Promise<void> {
-    const tasks = this.loadedPlugins.map(plugin => this.invokeOnReady(plugin));
-    await Promise.all(tasks);
-  }
-
-  private async invokeOnReady(plugin: any): Promise<void> {
-    if (typeof plugin.onReady !== "function") return;
-    try {
-      await plugin.onReady();
-    } catch (err) {
-      const id = plugin.pluginId || "unknown";
-      this.ctx.log.error(err, `Loader: Failed to execute onReady for plugin [${id}]`);
-    }
+    await bootstrapper.run(registry.getClassMap(), registry.getDepMap());
   }
 }
